e6be57c9a24332e665a0b29c8a92aec5f6ac12cd
wojdzie <wojdzi@outlook.com>
2021-05-13 19:55:37 +0200

    is paid bit field has been added

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -63,110 +63,6 @@ BEGIN
 END
 GO
 
--- Purchase.PurchaseOrderType
-CREATE TABLE Purchase.PurchaseOrderType (
-	id						BIGINT NOT NULL IDENTITY,
-	type					VARCHAR(50) NOT NULL,
-	createdDate				DATETIME NOT NULL DEFAULT GETDATE(),
-	createdBy				VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
-	lastModified			DATETIME NOT NULL DEFAULT GETDATE(),
-	lastModifiedBy			VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
-	PRIMARY KEY(id)
-)
-GO
-
-CREATE TRIGGER Purchase.PurchaseOrderTypeTrigger ON Purchase.PurchaseOrderType
-AFTER UPDATE
-AS
-BEGIN
-	UPDATE Purchase.PurchaseOrderType
-	SET lastModified = GETDATE(), lastModifiedBy = CURRENT_USER
-	WHERE id IN (
-		SELECT id FROM inserted
-	)
-END
-GO
-
--- Purchase.PurchaseOrder
-CREATE TABLE Purchase.PurchaseOrder (
-	id						BIGINT NOT NULL IDENTITY,
-    clientId				BIGINT NOT NULL,
-	typeId					BIGINT NOT NULL,
-	purchaseOrderDate		DATETIME NOT NULL DEFAULT GETDATE(),
-	pickupDate				DATETIME NULL,
-	createdDate				DATETIME NOT NULL DEFAULT GETDATE(),
-	createdBy				VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
-	lastModified			DATETIME NOT NULL DEFAULT GETDATE(),
-	lastModifiedBy			VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
-	PRIMARY KEY(id),
-	FOREIGN KEY(typeId) REFERENCES Purchase.PurchaseOrderType(id),
-    FOREIGN KEY(clientId) REFERENCES Client.Client(id),
-	CONSTRAINT CHK_pickupDate CHECK (pickupDate >= purchaseOrderDate)
-)
-GO
-
-CREATE TRIGGER Purchase.PurchaseOrderTrigger ON Purchase.PurchaseOrder
-AFTER UPDATE
-AS
-BEGIN
-	UPDATE Purchase.PurchaseOrder
-	SET lastModified = GETDATE(), lastModifiedBy = CURRENT_USER
-	WHERE id IN (
-		SELECT id FROM inserted
-	)
-END
-GO
-
--- Purchase.Menu
-CREATE TABLE Purchase.Menu (
-	id						BIGINT NOT NULL IDENTITY,
-	productId				BIGINT NOT NULL,
-	createdDate				DATETIME NOT NULL DEFAULT GETDATE(),
-	createdBy				VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
-	lastModified			DATETIME NOT NULL DEFAULT GETDATE(),
-	lastModifiedBy			VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
-	PRIMARY KEY(id),
-	FOREIGN KEY(productId) REFERENCES Product.Product(id)
-)
-GO
-
-CREATE TRIGGER Purchase.MenuTrigger ON Purchase.Menu
-AFTER UPDATE
-AS
-BEGIN
-	UPDATE Purchase.Menu
-	SET lastModified = GETDATE(), lastModifiedBy = CURRENT_USER
-	WHERE id IN (
-		SELECT id FROM inserted
-	)
-END
-GO
-
--- Purchase.PurchaseOrderMenu
-CREATE TABLE Purchase.PurchaseOrderMenu (
-    purchaseOrderId         BIGINT NOT NULL,
-    menuId                  BIGINT NOT NULL,
-    FOREIGN KEY(purchaseOrderId) REFERENCES Purchase.PurchaseOrder(id),
-    FOREIGN KEY(menuId) REFERENCES Purchase.Menu(id),
-    createdDate				DATETIME NOT NULL DEFAULT GETDATE(),
-    createdBy				VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
-    lastModified			DATETIME NOT NULL DEFAULT GETDATE(),
-    lastModifiedBy			VARCHAR(50) NOT NULL DEFAULT CURRENT_USER
-)
-GO
-
-CREATE TRIGGER Purchase.PurchaseOrderMenuTrigger ON Purchase.PurchaseOrderMenu
-AFTER UPDATE
-AS
-BEGIN
-UPDATE Purchase.PurchaseOrderMenu
-SET lastModified = GETDATE(), lastModifiedBy = CURRENT_USER
-WHERE id IN (
-    SELECT id FROM inserted
-)
-END
-GO
-
 -- Client.Address
 CREATE TABLE Client.Address (
 	id						BIGINT NOT NULL IDENTITY,
@@ -254,6 +150,114 @@ BEGIN
 	WHERE id IN (
 		SELECT id FROM inserted
 	)
+END
+GO
+
+-- Purchase.PurchaseOrderType
+CREATE TABLE Purchase.PurchaseOrderType (
+	id						BIGINT NOT NULL IDENTITY,
+	type					VARCHAR(50) NOT NULL,
+	createdDate				DATETIME NOT NULL DEFAULT GETDATE(),
+	createdBy				VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
+	lastModified			DATETIME NOT NULL DEFAULT GETDATE(),
+	lastModifiedBy			VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
+	PRIMARY KEY(id)
+)
+GO
+
+CREATE TRIGGER Purchase.PurchaseOrderTypeTrigger ON Purchase.PurchaseOrderType
+AFTER UPDATE
+AS
+BEGIN
+	UPDATE Purchase.PurchaseOrderType
+	SET lastModified = GETDATE(), lastModifiedBy = CURRENT_USER
+	WHERE id IN (
+		SELECT id FROM inserted
+	)
+END
+GO
+
+-- Purchase.PurchaseOrder
+CREATE TABLE Purchase.PurchaseOrder (
+	id						BIGINT NOT NULL IDENTITY,
+    clientId				BIGINT NOT NULL,
+	typeId					BIGINT NOT NULL,
+    paid				    BIT NOT NULL DEFAULT 0,
+	purchaseOrderDate		DATETIME NOT NULL DEFAULT GETDATE(),
+	pickupDate				DATETIME NULL,
+	createdDate				DATETIME NOT NULL DEFAULT GETDATE(),
+	createdBy				VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
+	lastModified			DATETIME NOT NULL DEFAULT GETDATE(),
+	lastModifiedBy			VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
+	PRIMARY KEY(id),
+	FOREIGN KEY(typeId) REFERENCES Purchase.PurchaseOrderType(id),
+    FOREIGN KEY(clientId) REFERENCES Client.Client(id),
+	CONSTRAINT CHK_pickupDate CHECK (pickupDate >= purchaseOrderDate),
+)
+GO
+
+CREATE TRIGGER Purchase.PurchaseOrderTrigger ON Purchase.PurchaseOrder
+AFTER UPDATE
+AS
+BEGIN
+	UPDATE Purchase.PurchaseOrder
+	SET lastModified = GETDATE(), lastModifiedBy = CURRENT_USER
+	WHERE id IN (
+		SELECT id FROM inserted
+	)
+END
+GO
+
+-- Purchase.Menu
+CREATE TABLE Purchase.Menu (
+	id						BIGINT NOT NULL IDENTITY,
+	productId				BIGINT NOT NULL,
+	price					DECIMAL(10,2) NOT NULL,
+	createdDate				DATETIME NOT NULL DEFAULT GETDATE(),
+	createdBy				VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
+	lastModified			DATETIME NOT NULL DEFAULT GETDATE(),
+	lastModifiedBy			VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
+	PRIMARY KEY(id),
+	FOREIGN KEY(productId) REFERENCES Product.Product(id)
+)
+GO
+
+CREATE TRIGGER Purchase.MenuTrigger ON Purchase.Menu
+AFTER UPDATE
+AS
+BEGIN
+	UPDATE Purchase.Menu
+	SET lastModified = GETDATE(), lastModifiedBy = CURRENT_USER
+	WHERE id IN (
+		SELECT id FROM inserted
+	)
+END
+GO
+
+-- Purchase.PurchaseOrderMenu
+CREATE TABLE Purchase.PurchaseOrderMenu (
+	id						BIGINT NOT NULL IDENTITY,
+    purchaseOrderId         BIGINT NOT NULL,
+    menuId                  BIGINT NOT NULL,
+	createdDate				DATETIME NOT NULL DEFAULT GETDATE(),
+    createdBy				VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
+    lastModified			DATETIME NOT NULL DEFAULT GETDATE(),
+    lastModifiedBy			VARCHAR(50) NOT NULL DEFAULT CURRENT_USER,
+	PRIMARY KEY(id),
+    FOREIGN KEY(purchaseOrderId) REFERENCES Purchase.PurchaseOrder(id),
+    FOREIGN KEY(menuId) REFERENCES Purchase.Menu(id)
+)
+GO
+
+CREATE TRIGGER Purchase.PurchaseOrderMenuTrigger ON Purchase.PurchaseOrderMenu
+AFTER UPDATE
+AS
+BEGIN
+UPDATE Purchase.PurchaseOrderMenu
+SET lastModified = GETDATE(), lastModifiedBy = CURRENT_USER
+WHERE id IN (
+    SELECT id FROM inserted
+)
 END
 GO
 
