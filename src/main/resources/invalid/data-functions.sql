96d4f7defe5650f63282555c4a97da4c3f3d6bb4
magdapieron <magdalena.pieron@opoczta.pl>
2021-06-13 23:38:54 +0200

    added new functions

--- src/main/resources/invalid/data-functions.sql
+++ src/main/resources/invalid/data-functions.sql
@@ -118,38 +118,64 @@ BEGIN
     RETURN @sum;
 END;
 
-CREATE FUNCTION Restaurant.AvailableTables(@data DATETIME)
+CREATE FUNCTION AvailableTables(@date DATETIME)
 	RETURNS INT
 AS
 BEGIN
 	DECLARE @localSquareMeters INT;
-	DECLARE @squareMetersLimit INT = NULL;
+	DECLARE @squareMetersLimitPerPerson INT = NULL;
 	DECLARE @notAvailable INT;
 	DECLARE @available INT;
+	DECLARE @all INT;
+	DECLARE @availableSeatsAfterRestriction INT;
 
 SELECT @localSquareMeters = localSquareMeters
 FROM Restaurant.Restaurant
 
-SELECT @squareMetersLimit = squareMetersLimit
-FROM StaticData.AreaRestriction
-WHERE startDate <= @data AND endDate >= @data
+SELECT @all = COUNT(seats)
+FROM Restaurant.SingleTable
 
-IF(@squareMetersLimit IS NOT NULL)	-- oblicza dostępne miejsca na podstawie restrykcji
+SELECT @squareMetersLimitPerPerson = squareMetersLimit
+FROM StaticData.AreaRestriction
+WHERE startDate <= @date AND endDate >= @date
+
+    SET @availableSeatsAfterRestriction = @localSquareMeters / @squareMetersLimitPerPerson
+
+	IF(@all > @availableSeatsAfterRestriction)
 BEGIN
-    SELECT @notAvailable = COUNT(*)
-    FROM Restaurant.SingleTable
-    WHERE available = 0
-    SET @available = ( @localSquareMeters / @squareMetersLimit) - @notAvailable*4
+SELECT @notAvailable = SUM(seats)
+FROM Restaurant.SingleTable
+         FULL JOIN Restaurant.Reservation
+                   ON Restaurant.SingleTable.id = Restaurant.Reservation.tableId
+WHERE reservationDate < @date AND DATEADD(HOUR, 2, reservationDate) > @date;
+
+SET @available = @availableSeatsAfterRestriction - @notAvailable
 END
 ELSE
-BEGIN
-    SELECT @available = COUNT(*)*4		-- nie ma restrykcji, więc liczba stolików*4miejsca
-    FROM Restaurant.SingleTable
-    WHERE available = 1
-END
+SELECT @available = COUNT(DISTINCT Restaurant.SingleTable.id)
+FROM Restaurant.SingleTable
+         LEFT JOIN Restaurant.Reservation
+                   ON Restaurant.SingleTable.id = Restaurant.Reservation.tableId
+WHERE reservationDate IS NULL OR (reservationDate > @date OR DATEADD(HOUR, 2, reservationDate) < @date);
+
 RETURN @available
 END
 
+CREATE FUNCTION CanOrderSeafood(
+    @orderDate DATETIME,
+    @reservationDate DATETIME
+)
+    RETURNS bit
+AS
+BEGIN
+   IF
+(DATEPART(WEEKDAY, @orderDate) = 5 AND DATEADD(DD, -2, @orderDate) < @reservationDate) OR
+	(DATEPART(WEEKDAY, @orderDate) = 6 AND DATEADD(DD, -3, @orderDate) < @reservationDate) OR
+	(DATEPART(WEEKDAY, @orderDate) = 7 AND DATEADD(DD, -4, @orderDate) < @reservationDate)
+		RETURN 1
 
+	RETURN 0
+END
+GO
 
 
--- src/main/resources/procedure.sql
+++ src/main/resources/procedure.sql
@@ -33,3 +33,366 @@ BEGIN
 			ORDER BY NEWID();
 		END;
 END;
+
+CREATE PROCEDURE Client.AddClient
+(
+@typeId	BIGINT,
+@firstName  VARCHAR(50) NULL,
+@lastName	VARCHAR(80) NULL,
+@companyName	VARCHAR(100) NULL,
+@NIP	VARCHAR(10) NULL,
+@Regon	VARCHAR(14) NULL,
+@phone	VARCHAR(9) NULL,
+@email	VARCHAR(50) NULL,
+@address	VARCHAR(100) NULL,
+@city	VARCHAR(50) NULL,
+@postalCode	VARCHAR(6) NULL,
+@country	VARCHAR(50) NULL
+)
+AS
+BEGIN
+DECLARE @addressID BIGINT
+EXEC Client.InsertAddress @address = @address,
+						@city = @city,
+						@postalCode = @postalCode,
+						@country = @country,
+						@id = @addressID OUTPUT;
+
+EXEC  Client.InsertClient @typeId = @typeId,
+						@firstName = @firstName,
+						@lastName = @lastName,
+						@companyName = @companyName,
+						@NIP = @NIP,
+						@Regon = @Regon,
+						@addressId = @addressId,
+						@phone = @phone,
+						@email = @email;
+END;
+
+CREATE PROCEDURE StaticData.AddRestrictions
+(
+    @startDate DATETIME,
+    @endDate DATETIME,
+    @newArea INT
+)
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+INSERT INTO StaticData.AreaRestriction (squareMetersLimit, startDate, endDate)
+VALUES (@newArea, @startDate, @endDate)
+END;
+
+CREATE PROCEDURE Restaurant.BusinessTableReservation
+(
+@date DATETIME,
+@peopleAmount INT,
+@companyName	VARCHAR(100),
+@NIP	VARCHAR(10),
+@Regon	VARCHAR(14),
+@phone	VARCHAR(9),
+@email	VARCHAR(50)
+)
+AS
+BEGIN
+	DECLARE @typeId BIGINT
+	DECLARE @clientId BIGINT
+
+SELECT @typeId = id FROM Client.ClientType WHERE type = 'business'
+
+    EXEC Client.AddClient
+		@typeId = @typeId,
+		@firstName = NULL,
+		@lastName = NULL,
+		@companyName = @companyName,
+		@NIP = @NIP,
+		@Regon = @Regon,
+		@phone = @phone,
+		@email = @email,
+		@address = NULL,
+		@city = NULL,
+		@postalCode = NULL,
+		@country = NULL,
+		@clientId = @clientId OUTPUT
+
+	EXEC Restaurant.TableReservation
+		@date = @date,
+		@clientId = @clientId,
+		@peopleAmount = @peopleAmount
+END;
+
+CREATE PROCEDURE Client.AddClient
+(
+@typeId	BIGINT,
+@firstName  VARCHAR(50) NULL,
+@lastName	VARCHAR(80) NULL,
+@companyName	VARCHAR(100) NULL,
+@NIP	VARCHAR(10) NULL,
+@Regon	VARCHAR(14) NULL,
+@phone	VARCHAR(9) NULL,
+@email	VARCHAR(50) NULL,
+@address	VARCHAR(100) NULL,
+@city	VARCHAR(50) NULL,
+@postalCode	VARCHAR(6) NULL,
+@country	VARCHAR(50) NULL,
+@clientId BIGINT OUTPUT
+)
+AS
+BEGIN
+DECLARE @addressID BIGINT = NULL
+
+IF(@address IS NOT NULL AND @city IS NOT NULL AND @postalCode IS NOT NULL AND @country IS NOT NULL)
+EXEC Client.InsertAddress @address = @address,
+						@city = @city,
+						@postalCode = @postalCode,
+						@country = @country,
+						@id = @addressID OUTPUT;
+
+EXEC  Client.InsertClient @typeId = @typeId,
+						@firstName = @firstName,
+						@lastName = @lastName,
+						@companyName = @companyName,
+						@NIP = @NIP,
+						@Regon = @Regon,
+						@addressId = @addressId,
+						@phone = @phone,
+						@email = @email,
+						@id = @clientId OUTPUT;
+END;
+
+CREATE PROCEDURE Restaurant.ConfirmReservation(
+    @reservationId BIGINT
+)
+    AS
+BEGIN
+UPDATE Restaurant.Reservation
+SET confirmed = 1
+WHERE id = @reservationId
+END
+GO
+
+
+CREATE PROCEDURE Restaurant.IndividualOrder(
+@peopleAmount INT
+)
+AS
+BEGIN
+	DECLARE @typeId BIGINT
+
+SELECT @typeId = id FROM Purchase.PurchaseOrderType WHERE type = 'in-place'
+
+    EXEC Restaurant.TableReservation
+		@date = GETDATE,
+		@clientId = NULL,
+		@peopleAmount = @peopleAmount
+
+INSERT INTO Purchase.PurchaseOrder("typeId", "paid")
+VALUES(@typeId, 0)
+END;
+
+CREATE PROCEDURE Restaurant.IndividualOrderTakeaway
+AS
+BEGIN
+	DECLARE @typeId BIGINT
+
+SELECT @typeId = id FROM Purchase.PurchaseOrderType WHERE type = 'takeaway'
+
+    INSERT INTO Purchase.PurchaseOrder("typeId", "paid", "pickupDate")
+VALUES(@typeId, 0, GETDATE())
+END;
+
+CREATE PROCEDURE Restaurant.IndividualOrderTakeawayInAdvance(
+@pickupDate DATETIME
+)
+AS
+BEGIN
+	DECLARE @typeId BIGINT
+
+SELECT @typeId = id FROM Purchase.PurchaseOrderType WHERE type = 'takeaway'
+
+    INSERT INTO Purchase.PurchaseOrder("typeId", "paid", "pickupDate")
+VALUES(@typeId, 0, @pickupDate)
+END;
+
+CREATE PROCEDURE Restaurant.IndividualTableReservation
+(
+@date DATETIME,
+@peopleAmount INT,
+@firstName VARCHAR(50),
+@lastName VARCHAR(80),
+@companyName	VARCHAR(100),
+@NIP	VARCHAR(10),
+@Regon	VARCHAR(14),
+@phone	VARCHAR(9),
+@email	VARCHAR(50),
+@address VARCHAR(100),
+@city VARCHAR(50),
+@postalCode VARCHAR(6),
+@country VARCHAR(50)
+)
+AS
+BEGIN
+	DECLARE @typeId BIGINT
+	DECLARE @clientId BIGINT
+
+SELECT @typeId = id FROM Client.ClientType WHERE type = 'individual'
+
+    EXEC Client.AddClient
+		@typeId = @typeId,
+		@firstName = @firstName,
+		@lastName = @lastName,
+		@companyName = @companyName,
+		@NIP = @NIP,
+		@Regon = @Regon,
+		@phone = @phone,
+		@email = @email,
+		@address = @address,
+		@city = @city,
+		@postalCode = @postalCode,
+		@country = @country,
+		@clientId = @clientId OUTPUT
+
+	EXEC Restaurant.TableReservation
+		@date = @date,
+		@clientId = @clientId,
+		@peopleAmount = @peopleAmount
+END;
+
+CREATE PROCEDURE Client.InsertAddress
+(
+    @address	VARCHAR(100),
+    @city	VARCHAR(50),
+    @postalCode	VARCHAR(6),
+    @country	VARCHAR(50),
+    @id BIGINT OUTPUT
+)
+    AS
+BEGIN
+INSERT INTO Client.Address(
+    address,
+    city,
+    postalCode,
+    country
+)
+VALUES (
+           @address,
+           @city,
+           @postalCode,
+           @country
+       )
+    SET @id = SCOPE_IDENTITY();
+END;
+
+CREATE PROCEDURE Client.InsertClient
+(@typeId	BIGINT,
+@firstName  VARCHAR(50),
+@lastName	VARCHAR(80),
+@companyName	VARCHAR(100),
+@NIP	VARCHAR(10),
+@Regon	VARCHAR(14),
+@addressId	BIGINT,
+@phone	VARCHAR(9),
+@email	VARCHAR(50),
+@id BIGINT OUTPUT
+)
+AS
+BEGIN
+INSERT INTO Client.Client(
+    typeId,
+    firstName,
+    lastName,
+    companyName,
+    NIP,
+    Regon,
+    addressId,
+    phone,
+    email
+)
+VALUES (
+           @typeId,
+           @firstName,
+           @lastName,
+           @companyName,
+           @NIP,
+           @Regon,
+           @addressId,
+           @phone,
+           @email
+       )
+
+    SET @id = SCOPE_IDENTITY()
+END;
+
+CREATE PROCEDURE Restaurant.TableReservation(
+	@date DATETIME,
+	@clientId BIGINT,
+	@peopleAmount INT
+)
+AS
+BEGIN
+
+	IF(dbo.AvailableTables(@date) >= @peopleAmount AND @peopleAmount >= 2)
+BEGIN
+		DECLARE @peopleLeft INT = @peopleAmount
+
+		WHILE @peopleLeft > 0
+BEGIN
+			DECLARE @freeTableId BIGINT;
+			DECLARE @seats INT;
+
+SELECT
+        @freeTableId = Restaurant.SingleTable.id,
+        @seats = seats
+FROM Restaurant.SingleTable
+         RIGHT JOIN Restaurant.Reservation
+                    ON Restaurant.SingleTable.id = Restaurant.Reservation.tableId
+WHERE reservationDate < @date AND DATEADD(HOUR, 2, reservationDate) > @date;
+
+INSERT INTO Restaurant.Reservation ("clientId", "tableId", "reservationDate")
+VALUES (@clientId, @freeTableId, @date)
+
+    SET @peopleLeft = @peopleLeft - @seats
+END
+END
+END
+
+CREATE PROCEDURE Restaurant.WorkerTableReservation
+(
+@date DATETIME,
+@peopleAmount INT,
+@firstName VARCHAR(50),
+@lastName VARCHAR(80),
+@companyName	VARCHAR(100),
+@NIP	VARCHAR(10),
+@Regon	VARCHAR(14),
+@phone	VARCHAR(9),
+@email	VARCHAR(50)
+)
+AS
+BEGIN
+	DECLARE @typeId BIGINT
+	DECLARE @clientId BIGINT
+
+SELECT @typeId = id FROM Client.ClientType WHERE type = 'worker'
+
+    EXEC Client.AddClient
+		@typeId = @typeId,
+		@firstName = @firstName,
+		@lastName = @lastName,
+		@companyName = @companyName,
+		@NIP = @NIP,
+		@Regon = @Regon,
+		@phone = @phone,
+		@email = @email,
+		@address = NULL,
+		@city = NULL,
+		@postalCode = NULL,
+		@country = NULL,
+		@clientId = @clientId OUTPUT
+
+	EXEC Restaurant.TableReservation
+		@date = @date,
+		@clientId = @clientId,
+		@peopleAmount = @peopleAmount
+END;
